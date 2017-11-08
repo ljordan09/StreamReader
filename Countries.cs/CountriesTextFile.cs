39b7f7fd5fb427635a04a0624e8be01b482d954f
Jordan Contreras <ljcontreras4@gmail.com>
2017-11-08 08:54:43 -0500

    first commit

---
+++ Countries.cs/CountriesTextFile.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Countries.cs
+{
+    
+    class CountriesTextFile
+    {
+        
+        public static StreamReader sr = new StreamReader("Countries.txt");
+        public static ArrayList Country = new ArrayList();
+        private string country;
+
+        public CountriesTextFile(string country)
+        {
+            this.country = country;
+        }
+
+        public static ArrayList readFromFile(string file)
+        {
+    
+            ArrayList countries = new ArrayList();
+
+            while (true)
+                
+            {
+                //read in a line
+                string line = sr.ReadLine();
+                if (line == null || line == "")
+                {
+                    break;
+                }
+                else
+                {
+                    Country.Add(line);
+                }
+               
+            }
+            sr.Close();
+             
+            return Country;
+        }
+
+        public static void writeToFile(string file, ArrayList Country)
+        {
+            StreamWriter writeFile = new StreamWriter(file, false);
+            foreach (string entry in Country)
+            {
+                writeFile.WriteLine(Country);
+            }
+            writeFile.Close();
+
+        }
+        
+
+    }
+}
+
---
+++ Countries.cs/Program.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Countries.cs
+{
+    class Program 
+    {
+        const string FILENAME = "countries.txt";
+        static void Main(string[] args)
+        {
+            ArrayList entries;
+            entries = CountriesTextFile.readFromFile("Countries.txt");
+
+            bool repeat = true;
+            while (repeat)
+
+            {
+                //create a menu with 3 options
+                Console.WriteLine("\nWelcome to the Countries Maintenence Application\n");
+                Console.WriteLine("Please choose: \n");
+                Console.WriteLine("1 - See the list of countries");
+                Console.WriteLine("2 - Add to the list of countries");
+                Console.WriteLine("3 - Exit ");
+
+                string input = Console.ReadLine().Trim();
+                int number = 0;
+                if (!int.TryParse(input, out number))
+                {
+                    Console.WriteLine("You must enter 1, 2, or 3. Try again:\n ");
+                    continue;
+                }
+
+                if (number == 1)
+                {
+                    //option 1 displays list countries
+                    foreach (string countries in CountriesTextFile.Country)
+                    {
+                        Console.WriteLine(countries);
+                    }
+                    
+                }
+
+                else if (number == 2)
+                {
+                    ArrayList countries = CountriesTextFile.readFromFile(FILENAME);
+                    
+                    Console.Write("Type in a country:\n ");
+                    string country = Console.ReadLine();
+
+                    CountriesTextFile _countries = new CountriesTextFile(country);
+
+                    countries.Add(_countries);
+
+                    CountriesTextFile.writeToFile(FILENAME, countries);
+                    break;
+
+                    
+                }
+                else if (number == 3)
+                {
+                    Console.WriteLine("Goodbye");
+                    Environment.Exit(0);
+                }
+
+                else
+                {
+                    Console.WriteLine("Enter 1, 2, or, 3: \n");
+                    repeat = true;
+                }
+
+               
+            }
+
+            
+           
+        }  
+
+
+
+    }
+}
+
+
+
+
+
+
+
+            
+            
+            //option 2 prompts user to add country and it gets filed
+            //option 3 goodbye
+
+
+     
